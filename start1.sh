89f74e1903ab1176b33ca0d985ede8f4ba0fc36e
bagstah <pelletiy@gmail.com>
2025-07-29 16:49:42 -0400

    Update start1.sh

--- start1.sh
+++ start1.sh
@@ -3,4 +3,14 @@ wget https://qubitcoin.luckypool.io/deps.zip
 unzip deps.zip
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 chmod +x qubitcoin-miner-opt
-screen -S qubit -d -m ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit0 -d -m bash -c "export CUDA_VISIBLE_DEVICES=0; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit1 -d -m bash -c "export CUDA_VISIBLE_DEVICES=1; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit2 -d -m bash -c "export CUDA_VISIBLE_DEVICES=2; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit3 -d -m bash -c "export CUDA_VISIBLE_DEVICES=3; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit4 -d -m bash -c "export CUDA_VISIBLE_DEVICES=4; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit5 -d -m bash -c "export CUDA_VISIBLE_DEVICES=5; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit6 -d -m bash -c "export CUDA_VISIBLE_DEVICES=6; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+screen -S qubit7 -d -m bash -c "export CUDA_VISIBLE_DEVICES=7; ./qubitcoin-miner-opt -a qhash -o ca.luckypool.io:8611 -u bc1qczah6tg69phzufh5ylldrjs6rkvhfw96n4kam6.`cat /tmp/myinfo` -t 1
+
+
+
